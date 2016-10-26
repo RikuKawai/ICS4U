6bb1e2607ecf09f1f41b032ad52f031c34587bad
Quinlan <riku@thisis.moe>
2016-10-26 14:59:43 -0400

    added comments

--- src/mcnellen/Recursion.java
+++ src/mcnellen/Recursion.java
@@ -1,11 +1,23 @@
 package mcnellen;
 
+/**
+ * Recursion.java
+ * Various examples of recursive methods
+ * @author Quinlan McNellen
+ * @date 10/26/2016
+ */
 public class Recursion {
 
 	public static void main(String[] args) {
 		starString(6);
 		System.out.println(permut(10,4));
+		System.out.println(factorial(8));
 	}
+	
+	/**
+	 * Prints n rows of asterisks, each with 2n-1 asterisks
+	 * @param n Number of iterations
+	 */
 	public static void starString(int n) {
 		if (n >= 0) {
 			starString(n-1);
@@ -17,11 +29,22 @@ public class Recursion {
 		}
 		return;
 	}
+	/**
+	 * Determines the factorial of n
+	 * @param n
+	 * @return calculated factorial
+	 */
 	public static int factorial(int n) {
 		if (n==1) {
 			return 1;
 		} return n*factorial(n-1);
 	}
+	/**
+	 * Determines the number of permutations of a set
+	 * @param n Number of independent values
+	 * @param r Values per set
+	 * @return
+	 */
 	public static int permut(int n, int r) {
 		return factorial(n)/factorial(n-r);
 	}
