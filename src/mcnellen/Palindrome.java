e4b580755c72bb3cb2430cd5ba9e511268079949
BuildTools <unconfigured@null.spigotmc.org>
2016-11-14 18:13:12 -0500

    fixed method titles, moved printing to a third method

--- src/mcnellen/Palindrome.java
+++ src/mcnellen/Palindrome.java
@@ -6,28 +6,30 @@ public class Palindrome {
 	 * Palindrome.java
 	 * Recursive Palindrome tester
 	 * @author Quinlan McNellen
-	 * @version 1.0.0
-	 * @date 11/09/2016
+	 * @version 1.1.0
+	 * @date 11/14/2016
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Palindrome("some random string");
-		Palindrome("racecar");
-		Palindrome("The quick brown fox jumps over the lazy dog");
-		Palindrome("madam");
-		Palindrome("kayak");
+		printResult(palindrome("some random string"));
+		printResult(palindrome("racecar"));
+		printResult(palindrome("The quick brown fox jumps over the lazy dog"));
+		printResult(palindrome("madam"));
+		printResult(palindrome("kayak"));
+	}
+	public static void printResult(boolean bool) {
+		if (bool) {
+			System.out.println("This string is a palindrome.");
+		} else if (!bool) {
+			System.out.println("This string is not a palindrome.");
+		}
 	}
 	/**
 	 * Palindrome checker control method
 	 * @param str	String to test
 	 */
-	public static void Palindrome(String str) {
-		boolean palindrome = Palindrome(str, 0, str.length() - 1);
-		if (palindrome) {
-			System.out.println("This string is a palindrome.");
-		} else if (!palindrome) {
-			System.out.println("This string is not a palindrome.");
-		}
+	public static boolean palindrome(String str) {
+		return palindrome(str, 0, str.length() - 1);
 	}
 	/**
 	 * Palindrome checker recursive method
@@ -36,9 +38,9 @@ public class Palindrome {
 	 * @param right	Right character
 	 * @return		True if palindrome, false if not
 	 */
-	private static boolean Palindrome(String str, int left, int right) {
+	private static boolean palindrome(String str, int left, int right) {
 		if (str.charAt(left) == str.charAt(right) && left < right) {
-			Palindrome(str, left + 1, right - 1);
+			palindrome(str, left + 1, right - 1);
 			return true;
 		} else if (left < right) {
 			return false;
